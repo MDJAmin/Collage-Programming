83b1d8e83beb42b54a84a13791d06369dd42a643
MDJAmin <mohammadamindf@gmail.com>
2024-11-16 20:40:29 +0330

    for traning in py

---
+++ 2/py.py
@@ -0,0 +1,10 @@
+n = int(input("enter the number: "))
+
+sum_numbers = 0
+
+for i in range(n):
+    number = float(input(f"enter {i + 1} number "))
+    sum_numbers += number  
+
+print(f"clac is: {sum_numbers}")
+
